c2afbc503a804b0b0bf3f10543d3ca5c6c2309e1
GulKaratas <glsrn_7872@outlook.com>
2025-12-18 15:18:02 +0300

    Add dynamic blog sections and partial views to homepage
    Refactored IndexController and Index.cshtml to display dynamic blog content using data from the database. Added five new partial views (Partial1–Partial5) for various blog data presentations. Updated project file to include new partials. Localized UI text to Turkish and updated footer information. Static homepage content replaced with data-driven sections for a more interactive travel blog experience.

--- TravelTripProject/Controllers/IndexController.cs
+++ TravelTripProject/Controllers/IndexController.cs
@@ -3,19 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Models.Class;
 
 namespace TravelTripProject.Controllers
 {
     public class IndexController : Controller
     {
-        // GET: Index
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.Take(5).ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+           var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var degerler = c.Blogs.Where(x => x.Id == 3).ToList();  
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial3()
+        {
+            var degerler = c.Blogs.ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial4()
+        {
+            var degerler = c.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial5()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+            return PartialView(degerler);
         }
     }
 }
